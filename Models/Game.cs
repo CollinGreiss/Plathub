92a68b1c2705a9edd4e32916bb65270c7387e0a2
Seth Kimberlin <skimberlin04@gmail.com>
2024-02-21 09:23:41 -0700

    Basic Models

---
+++ Models/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace Plathub.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        
+    }
+}
---
+++ Models/UserGame.cs
@@ -0,0 +1,8 @@
+﻿namespace Plathub.Models
+{
+    public class UserGame
+    {
+        public string UserId { get; set; }
+        public int GameId { get; set; }
+    }
+}
