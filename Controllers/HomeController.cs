1599cc86bf43466043f7d430549d0f50e63b74ea
NeuHezxp <mpatino@student.neumont.edu>
2024-02-22 18:04:49 -0700

    added titles to partial and library

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -41,8 +41,12 @@ public class HomeController : Controller {
     return View();
     
   }
+    public IActionResult Library()
+    {
+        return View();
+    }
 
-	[ResponseCache( Duration = 0, Location = ResponseCacheLocation.None, NoStore = true )]
+    [ResponseCache( Duration = 0, Location = ResponseCacheLocation.None, NoStore = true )]
 	public IActionResult Error() {
 
 		return View( new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier } );
